6dc9f448be1c1e9f84cb6774acc00051bbc76bbe
Vladimir_Z <vovik5545@gmail.com>
2021-05-06 19:55:55 +0400

    Commit from 6 May 2021
    +ADD new custom context menu (react-contextmenu)

--- src/components/Content/Groups/Groups.jsx
+++ src/components/Content/Groups/Groups.jsx
@@ -6,13 +6,24 @@ import GroupHead from './GroupHead/GroupHead';
 import CreateGroupForm from '../Forms/CreateGroupForm/CreateGroupForm';
 import '../../../styles/transitions/_formGroup.scss';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
+import { FaRegCopy, FaList, FaEllipsisV, FaShareAlt } from 'react-icons/fa';
+import { RiSendPlaneFill, RiDeleteBin6Line } from 'react-icons/ri';
+import '../../../styles/contextMenu.scss'
 
 const Groups = (props) => {
 
+   const handleContextMenu = (e, data, target) => {
+      switch (data.action) {
+         default:
+            return
+         case 'console':
+            console.log(target.getAttribute('groupid'));
+      }
+   }
+
    const [showingGroupId, setShowingGroupId] = useState([]);
    const [isEditMode, setIsEditMode] = useState([]);
-
 
    const showEditMode = (groupId) => {
       if (isEditMode.includes(groupId)) {
@@ -40,15 +51,20 @@ const Groups = (props) => {
                   <div
                      key={group.id}
                   >
-                     <GroupHead
-                        key={group.id}
-                        showGroupNotes={showGroupNotes}
-                        showEditMode={showEditMode}
-                        delGroup={props.delGroup}
-                        groupId={group.id}
-                        groupName={group.groupName}
-                        countNotes={group.groupNotes.length}
-                     />
+                     <ContextMenuTrigger
+                        id="contextmenu"
+                        attributes={{ groupid: group.id }}
+                     >
+                        <GroupHead
+                           key={group.id}
+                           showGroupNotes={showGroupNotes}
+                           showEditMode={showEditMode}
+                           delGroup={props.delGroup}
+                           groupId={group.id}
+                           groupName={group.groupName}
+                           countNotes={group.groupNotes.length}
+                        />
+                     </ContextMenuTrigger>
 
                      {/*  Форма добавления ноты */}
 
@@ -79,7 +95,7 @@ const Groups = (props) => {
                            ?
                            group.groupNotes.map(note => (
                               <CSSTransition
-                                 timeout={400}
+                                 timeout={300}
                                  classNames="card"
                                  key={note.id}
                               >
@@ -98,6 +114,47 @@ const Groups = (props) => {
                   </div>
                ))
          }
+         <ContextMenu
+            id="contextmenu"
+         >
+            <MenuItem
+               onClick={handleContextMenu}
+               data={{ action: 'console' }}
+            >
+               <FaRegCopy className="copy" />
+               <span>Напиши своё айди</span>
+            </MenuItem>
+            <MenuItem
+               onClick={handleContextMenu}
+            >
+               <FaEllipsisV className="openwith" />
+               <span>Open with</span>
+            </MenuItem>
+            <MenuItem
+               onClick={handleContextMenu}
+            >
+               <FaList className="watchlist" />
+               <span>Add to watchlist</span>
+            </MenuItem>
+            <MenuItem
+               onClick={handleContextMenu}
+            >
+               <RiSendPlaneFill className="send" />
+               <span>Send</span>
+            </MenuItem>
+            <MenuItem
+               onClick={handleContextMenu}
+            >
+               <RiDeleteBin6Line className="delete" />
+               <span>Delete</span>
+            </MenuItem>
+            <MenuItem
+               onClick={handleContextMenu}
+            >
+               <FaShareAlt className="share" />
+               <span>Share</span>
+            </MenuItem>
+         </ContextMenu>
       </div >
    )
 }
