87bc6425c6ece2452bc02104169788327ad81341
Vladimir <vovik5545@gmail.com>
2021-06-16 19:13:11 +0300

    Commit FIX 16 June +FIX firebase config

--- src/API/firebase.js
+++ src/API/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth'
 
 const app = firebase.initializeApp({
-   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-   appId: process.env.REACT_APP_FIREBASE_APP_ID
+   apiKey: 'AIzaSyA8RswvZCAak0P_-3ZSrsCabKANjWI8rbU',
+   authDomain: 'note-app-react-e52b6.firebaseapp.com',
+   projectId: 'note-app-react-e52b6',
+   storageBucket: 'note-app-react-e52b6.appspot.com',
+   messagingSenderId: '430373734501',
+   appId: '1:430373734501:web:5a0a7f0b70192ed6cc1e8c'
 })
 
 export const auth = app.auth();
