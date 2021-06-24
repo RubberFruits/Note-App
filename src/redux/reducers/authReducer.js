ef30596711d0c5ff782a40e76cff461acf0f44f9
RubberFruits <63595273+RubberFruits@users.noreply.github.com>
2021-06-24 19:24:15 +0300

    Update authReducer.js

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -27,3 +27,42 @@ export const setUserInState = (email, isAuth = true) => (
       isAuth
    }
 )
+
+const toggleFetching = (isFetching) => (
+   {
+      type: TOGGLE_FETCH,
+      isFetching
+   }
+)
+
+export const authentication = () => async (dispatch) => {
+   const response = await UtilityAPI.authUser();
+   if (response.data.resultCode === 0) {
+      let { id, email, login } = response.data.data;
+      dispatch(setAuthUserData(id, email, login, true));
+   }
+}
+
+export const login = (login, password, rememberMe) => async (dispatch) => {
+   dispatch(toggleFetching(true));
+   const response = await UtilityAPI.login(login, password, rememberMe)
+   if (response.data.resultCode === 0) {
+      dispatch(authentication());
+   } else {
+      let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
+      dispatch(stopSubmit('login',
+         {
+            _error: message
+         }
+      ));
+      dispatch(toggleFetching(false));
+   }
+}
+
+export const logOut = () => async (dispatch) => {
+   dispatch(toggleFetching());
+   const response = await UtilityAPI.logout()
+   if (response.data.resultCode === 0) {
+      dispatch(setAuthUserData(null, null, null, false));
+   }
+}
